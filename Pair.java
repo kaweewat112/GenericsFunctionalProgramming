bcd5d80c4c92d49a724704529f48bdc8d1fa204b
kaweewat112 <kaweewat.c@ku.th>
2026-09-17 17:26:22 +0700

    Add files via upload

--- Pair.java
+++ Pair.java
@@ -1,7 +1,24 @@
+import java.util.ArrayList;
+
 /**
  * TODO: ทำให้คลาสนี้เป็น Generic <K, V> ที่สามารถเก็บอ็อบเจกต์ได้ 2 ชนิด
  */
-public class Pair {
+public class Pair<k,v>{
+    
+    private final k key;
+    private final v value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
     // TODO: สร้างฟิลด์ private final สำหรับ key และ value
     
     // TODO: สร้าง Constructor ที่รับ key และ value
